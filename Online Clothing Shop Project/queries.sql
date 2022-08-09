21dce6473f179ddadfb4243d8117e47a4839ac2a
Jamarlon Young <109626866+JYoung2022@users.noreply.github.com>
2022-08-08 19:28:57 -0500

    Update queries.sql

--- Online Clothing Shop Project/queries.sql
+++ Online Clothing Shop Project/queries.sql
@@ -1,6 +1,6 @@
 -- Queries 
 
--- Finds all customers ordered by sex then first name
+-- Finds all customers ordered by gender then first name
 SELECT *
 from customer
 ORDER BY gender, first_name;
@@ -9,7 +9,7 @@ ORDER BY gender, first_name;
 SELECT first_name, customer.last_name
 FROM customer;
 
--- Finds out all the different genders
+-- Finds out all the different genders of customers
 SELECT DISTINCT gender
 FROM customer;
 
@@ -22,14 +22,6 @@ WHERE gender = 'Male';
 SELECT *
 FROM clothing
 WHERE (c_price <= '40.00');
-
-
--- Find names of all employees who have sold over 50,000
-SELECT employee.first_name, employee.last_name
-FROM employee
-WHERE employee.emp_id IN (SELECT works_with.emp_id
-                          FROM works_with
-                          WHERE works_with.total_sales > 50000);
 
 -- Finds the login_id of all customers who have spent more than 50.00 dollars
 SELECT customer.customer_id
